14f85b5eb8b456f70514096687491055a9e2bfad
simplyana08 <ana.t08@yahoo.com>
2025-05-15 13:42:15 +0200

    header gemaakt

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
+    <title>Family Travel</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-</body>
-</html>
+<header>
+    <div class="logo">
+        <img src="images/home-icon.svg" alt="Logo" width="40">
+        <span class="logo-text">Family Travel</span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">Over ons</a></li>
+            <li><a href="reizen.php">Reizen</a></li>
+            <li><a href="detail.php">Reis detailpagina</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
