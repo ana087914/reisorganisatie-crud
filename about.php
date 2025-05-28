e7b393d5db0b987bf732349d36648f4972cdca0a
ahawaaye <ahawa004@gmail.com>
2025-05-28 09:53:51 +0200

    OverOns pagina verwerking
    -header
    -pagina html en css

--- about.php
+++ about.php
@@ -12,7 +12,35 @@ include 'includes/header.php';
     <title>Family Travel-Overons</title>
 </head>
 <body>
-    
+
+    <section class= hero>
+       <h1>Over ons</h1>
+       <div class="scroll-down">&#8595;</div>
+    </section>
+
+    <section class="info-section">
+        <h2> Wij zijn beschikbaar voor iedere fase van uw ries</h2>
+        <p>
+            Wij Weten hoe moeilijk het is om een reis te plannen. Wij hebben veel reizen gemaakt en willen onze ervaringen gebruiken om jou reis te plannen!
+        </p>
+    </section>
+
+    <section class="insipration">
+        <img src="" alt="">
+        <div class="inpiration-tekst">
+            <p>
+               Laat u door ons inspireren. Iedereen verdien een adembenemnde reis, prachtige stedentrip of een zonvakantie.
+               Wij zoeken naar unieke deals en combineren deze. U vindt eenvoudig de beste opties!
+            
+            </p>
+            <button>
+                klik voor meer
+            </button>
+            
+        </div>
+    </section>
+
+
 </body>
 </html>
 
